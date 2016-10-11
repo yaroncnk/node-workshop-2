3d69faec6a398ae18796a167b72d340fd261831a
Yaron Cohen <yaroncnk@gmail.com>
2016-10-11 18:04:51 +0000

    first commit

---
+++ request-as-json.js
@@ -0,0 +1,25 @@
+//request JSON function 
+var request = require('request');
+
+function cb(value1,value2) {
+    console.log(value2);
+}
+
+function getValue(cb) {
+    request('https://api.wheretheiss.at/v1/satellites/25544', function(err, result) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            try {
+                cb(null, JSON.parse(result.body));
+
+            }
+            catch (error) {
+                cb(error);
+            }
+        }
+    });
+}
+
+getValue(cb);
